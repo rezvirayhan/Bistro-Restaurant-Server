7ab420b1ea3af3611df1d727a41cd23d5294172b
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-11-10 13:25:12 +0600

    added commit json web token install done by only install

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
 
 // MIDDLE WARE 
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Collection } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kpsrg5b.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,37 +24,52 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect()
 
-
-        // COLLECTION LIST
+        // ALL COLLECTION LIST
         const userCollection = client.db('bistroDb').collection("users")
         const menuCollection = client.db('bistroDb').collection("menu")
         const reviewsCollection = client.db('bistroDb').collection("reviews")
         const cartCollection = client.db('bistroDb').collection("cart")
 
-
-
-
-        // USER APII 
-        app.post("/users", async (req, res) => {
-            const user = req.body;
-            const result = await userCollection.insertOne(user)
+        // USER APII COLLECTION
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
             res.send(result)
         })
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ massage: 'Alrady Have an user' })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        });
 
-
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+        // MENU Collection
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
-        })
+        });
 
+        // REVIEW Collection 
         app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray()
             res.send(result)
-        })
-
+        });
 
         // CART CALLECTION ADDED 
         app.get('/carts', async (req, res) => {
@@ -66,6 +82,7 @@ async function run() {
             const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
+
         app.post('/carts', async (req, res) => {
             const item = req.body;
             const result = await cartCollection.insertOne(item)
@@ -77,15 +94,12 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query)
             res.send(result)
-        })
+        });
 
-
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 run().catch(console.dir);
