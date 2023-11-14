1018a4c30ac4a9932c4d6accc12209f0510cb435
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-11-14 15:56:21 +0600

    added dashbord deleted not yeat

--- index.js
+++ index.js
@@ -85,8 +85,6 @@ async function run() {
             res.send(result)
         });
 
-
-
         app.get('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email
             if (req.decoded.email !== email) {
@@ -120,6 +118,20 @@ async function run() {
             const result = await menuCollection.find().toArray()
             res.send(result)
         });
+
+
+        app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem)
+            res.send(result)
+        })
+
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // REVIEW Collection 
         app.get('/reviews', async (req, res) => {
